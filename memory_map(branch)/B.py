38339a0e99a1ca4999aa55f6a172d62010ce8448
Yorfrank Bastidas <ykbastidasc@gmail.com>
2019-11-21 00:00:56 -0400

    Synchronization between processes
    B program run and then wait for A to finish to continue.

--- memory_map(branch)/B.py
+++ memory_map(branch)/B.py
@@ -2,14 +2,30 @@ import binascii
 import random
 import sys
 import mmap
+import subprocess
+
+# call to  the A process
+p = subprocess.Popen(['python.exe', './A.py', '16'])
+
+# things to do while the A process finish
+empty_str = ""
+xor_list = []
+key_pos = 0
+binary_pos = 0
 seed = int(sys.argv[1])
+random.seed(seed)
+
+# wait for the A process to finish
+p.wait()
+
+#continue when the A process finishes
 with open("map.txt", "r+") as f:
     mm = mmap.mmap(f.fileno(), 0)
     bin_str = mm.readline().decode()
     mm.close()
 list_bin_str = list(bin_str)
 print("Texto Encriptado: \n" + bin_str)
-random.seed(seed)
+
 random_key = random.getrandbits(len(bin_str))
 key= '{0:0b}'.format(random_key)
 list_key = list(key)
@@ -18,12 +34,10 @@ lenght_bin = len(bin_str)
 while lenght_key < lenght_bin :
        list_key.insert(0,'0')
        lenght_key+=1
-empty_str = ""
+
 key = empty_str.join(list_key)
 
-xor_list = []
-key_pos = 0
-binary_pos = 0
+
 for binary_content in list_bin_str:
     if list_bin_str[binary_pos] != list_key[key_pos]:
         xor_list.append("1")
