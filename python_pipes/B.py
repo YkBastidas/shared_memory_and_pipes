bd978edf93be13bd471713c806bb7ad0b35d7965
Yorfrank Bastidas <ykbastidasc@gmail.com>
2019-11-27 00:29:33 -0400

    Deleting unused lines and test Prints

--- python_pipes/B.py
+++ python_pipes/B.py
@@ -1,13 +1,9 @@
-import binascii
+﻿import binascii
 import random
 import sys
 import os
 from subprocess import Popen, PIPE
 
-class Result:
-    pass
-
-result = Result()
 process_A = Popen(['python3', 'A.py', sys.argv[1]], stdout = PIPE)
 
 # things to do while the A process finish
@@ -20,17 +16,13 @@ random.seed(seed)
 
 process_A.wait()
 stdout = process_A.communicate()
-print(str(stdout[0]))
 bin_str = str(stdout[0])
-
 separator_index= bin_str.find(',')
 end_index= bin_str.find('.')
 
 len_original_bin = int(bin_str[separator_index+1:end_index])
 bin_str = bin_str[2:separator_index]
 list_bin_str = list(bin_str)
-print(bin_str)
-input ()
 len_bin_str = len(bin_str)
 while(len_bin_str<len_original_bin):
     list_bin_str.insert(0,'0')
@@ -60,8 +52,6 @@ if bin_str != "":
         key_pos +=1
     original_bin = empty_str.join(xor_list)
     original_string = int('0b'+original_bin,2).to_bytes((int('0b'+original_bin,2).bit_length() + 7) // 8, 'big').decode()
-    print("Texto Original : " + str(original_string)) 
-    if os.path.exists("memory_map"):
-        os.remove("memory_map")
+    print("Texto Original : " + str(original_string))
 else:
     print("ERROR: NO SE DETECTÓ LA ENTRADA DE DATOS")
